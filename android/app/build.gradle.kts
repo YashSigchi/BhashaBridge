7c83321462f8eaa276552fda1e453d15fca57928
Yash Sigchi <yashsigchi@Daredevil.local>
2025-07-30 00:00:12 +0530

    Updated android sdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.myapp"
     // compileSdk = flutter.compileSdkVersion
-    compileSdk = 34 
+    compileSdk = 35 
     // ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
@@ -31,8 +31,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // minSdk = flutter.minSdkVersion
         minSdk = 23
-        // targetSdk = flutter.targetSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
